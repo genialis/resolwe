e67ab35f1820674e7b2a2b3ef3c6723a4534f7d8
Miha Stajdohar <miha.stajdohar@gmail.com>
2015-04-09 11:54:27 -0500

    Created python package

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    name='resolwe',
+    version='0.9',
+    url='https://github.com/genialis/resolwe',
+    author='Genialis d.o.o.',
+    author_email='info@genialis.com',
+    description='Open source enterprise dataflow engine in Django.',
+    license='Apache License (2.0)',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'flow',
+        'apps',
+    ],
+    install_requires=[
+        'django>=1.8',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
