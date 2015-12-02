2709792431b58a77ec271ad01f2eccc320248d8c
Tadej Janež <tadej.j@nez.si>
2015-12-02 15:54:56 +0100

    Revert "Updated django-guardian dependency to fix test errors with Python 2.7."
    This reverts commit 51c043e4a5f2195fd7b0ee8cbeb263391bcf016a.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'django>=1.8',
         'djangorestframework>=3.1',
-        'django-guardian>=1.3.1',
+        'django-guardian==1.3',
         'django-filter>=0.9.2',
         'django-pgjsonb>=0.0.16',
         'django-jenkins==0.17.0',
