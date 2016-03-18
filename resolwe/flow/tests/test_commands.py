bebb29a94eb207f93ae03e57a5ab79e8b4fcfe6e
Domen Blenkuš <domen@blenkus.com>
2016-03-18 09:53:15 +0100

    Rename process_register to register

--- resolwe/flow/tests/test_commands.py
+++ resolwe/flow/tests/test_commands.py
@@ -19,37 +19,37 @@ class ProcessRegisterTest(TestCase):
 
     def test_process_register_all(self):
         out, err = StringIO(), StringIO()
-        call_command('process_register', path=PROCESSES_DIR, stdout=out, stderr=err)
+        call_command('register', path=PROCESSES_DIR, stdout=out, stderr=err)
         self.assertTrue('Inserted test-min' in out.getvalue())
         self.assertTrue('Skip processor test-min: newer version installed' in err.getvalue())
 
         out, err = StringIO(), StringIO()
-        call_command('process_register', path=PROCESSES_DIR, stdout=out, stderr=err)
+        call_command('register', path=PROCESSES_DIR, stdout=out, stderr=err)
         self.assertTrue('Skip processor test-min: same version installed' in out.getvalue())
         self.assertTrue('Skip processor test-bloated: same version installed' in out.getvalue())
         self.assertTrue('Skip processor test-min: newer version installed' in err.getvalue())
 
         out, err = StringIO(), StringIO()
-        call_command('process_register', path=PROCESSES_DIR, force=True, stdout=out, stderr=err)
+        call_command('register', path=PROCESSES_DIR, force=True, stdout=out, stderr=err)
         self.assertTrue('Updated test-min' in out.getvalue())
         self.assertTrue('Updated test-bloated' in out.getvalue())
         self.assertTrue('Skip processor test-min: newer version installed' in err.getvalue())
 
     def test_process_register_filter(self):
         out, err = StringIO(), StringIO()
-        call_command('process_register', path=PROCESSES_DIR, schemas='test-bloated', stdout=out, stderr=err)
+        call_command('register', path=PROCESSES_DIR, schemas='test-bloated', stdout=out, stderr=err)
         self.assertTrue('Inserted test-bloated' in out.getvalue())
         self.assertTrue('Inserted test-min' not in out.getvalue())
         self.assertEqual('', err.getvalue())
 
         out, err = StringIO(), StringIO()
-        call_command('process_register', path=PROCESSES_DIR, schemas='test-bloated', stdout=out, stderr=err)
+        call_command('register', path=PROCESSES_DIR, schemas='test-bloated', stdout=out, stderr=err)
         self.assertTrue('Skip processor test-bloated: same version installed' in out.getvalue())
         self.assertEqual('', err.getvalue())
 
         out, err = StringIO(), StringIO()
         call_command(
-            'process_register', path=PROCESSES_DIR, schemas='test-bloated', force=True, stdout=out, stderr=err)
+            'register', path=PROCESSES_DIR, schemas='test-bloated', force=True, stdout=out, stderr=err)
         self.assertTrue('Updated test-bloated' in out.getvalue())
         self.assertEqual('', err.getvalue())
 
@@ -58,5 +58,5 @@ class ProcessRegisterTestNoAdmin(TestCase):
 
     def test_process_register_no_admin(self):
         err = StringIO()
-        self.assertRaises(SystemExit, call_command, 'process_register', path=PROCESSES_DIR, stderr=err)
+        self.assertRaises(SystemExit, call_command, 'register', path=PROCESSES_DIR, stderr=err)
         self.assertEqual('Admin does not exist: create a superuser\n', err.getvalue())
