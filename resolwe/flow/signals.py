819323f746eb41ed5b5170cfab4f11f41df671ec
Domen Blenkuš <domen@blenkus.com>
2016-07-22 13:43:59 +0200

    Set verbosity to 0 in signals

--- resolwe/flow/signals.py
+++ resolwe/flow/signals.py
@@ -8,4 +8,4 @@ from resolwe.flow.managers import manager
 @receiver(post_save, sender=Data)
 def manager_post_save_handler(sender, instance, **kwargs):
     if instance.status == Data.STATUS_DONE or instance.status == Data.STATUS_ERROR:
-        manager.communicate()
+        manager.communicate(verbosity=0)
