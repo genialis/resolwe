3814dc5334803e6fd4570cea6c5c0265d5132efe
Jernej Kos <jernej@kos.mx>
2017-01-06 12:27:57 +0100

    Add ES viewsets and router

---
+++ resolwe/elastic/routers.py
@@ -0,0 +1,23 @@
+"""Search router."""
+from rest_framework.routers import Route, DefaultRouter
+
+
+class SearchRouter(DefaultRouter):
+    """Custom router for search endpoints.
+
+    Search endpoints don't follow REST principles and thus don't need
+    routes that default router provides.
+
+    """
+
+    routes = [
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+                'post': 'list_with_post'
+            },
+            name='{basename}',
+            initkwargs={}
+        )
+    ]
--- resolwe/elastic/viewsets.py
+++ resolwe/elastic/viewsets.py
@@ -15,8 +15,14 @@ from elasticsearch_dsl.query import Q
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-__all__ = ('ElasticSearchMixin',)
+__all__ = (
+    'ElasticSearchMixin',
+    'PaginationMixin',
+    'ElasticSearchBaseViewSet',
+)
 
 
 class ElasticSearchMixin(object):
@@ -33,13 +39,22 @@ class ElasticSearchMixin(object):
     ordering_fields = []
     ordering = None
 
+    def get_query_param(self, key, default=None):
+        """Get query parameter uniformly for GET and POST requests."""
+        value = self.request.query_params.get(key, None)
+        if value is None:
+            value = self.request.data.get(key, None)
+        if value is None:
+            value = default
+        return value
+
     def order_search(self, search):
         """Order given search by the ordering parameter given in request.
 
         :param search: ElasticSearch query object
 
         """
-        ordering = self.request.query_params.get('ordering', self.ordering)
+        ordering = self.get_query_param('ordering', self.ordering)
 
         ordering_field = ordering.lstrip('-')
         if ordering_field not in self.ordering_fields:
@@ -54,9 +69,13 @@ class ElasticSearchMixin(object):
 
         """
         for field in self.filtering_fields:
-            value = self.request.query_params.get(field, None)
+            value = self.get_query_param(field, None)
             if value:
-                search = search.query('wildcard', **{field: value})
+                if isinstance(value, list):
+                    filters = [Q('match', **{field: item}) for item in value]
+                    search = search.query('bool', should=filters)
+                else:
+                    search = search.query('wildcard', **{field: value})
 
         return search
 
@@ -80,3 +99,64 @@ class ElasticSearchMixin(object):
 
         # `minimum_should_match` is set to 1 by default
         return search.query('bool', should=filters)
+
+
+class PaginationMixin(object):
+    """Mixin for making paginated response in case pagination parameters are provided."""
+
+    def paginate_response(self, queryset):
+        """Optionally return paginated response.
+
+        If pagination parameters are provided in the request, then paginated response
+        is returned, otherwise response is not paginated.
+
+        """
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ElasticSearchBaseViewSet(PaginationMixin, ElasticSearchMixin, GenericViewSet):
+    """Base ViewSet for ElasticSearch based views.
+
+    This ViewSet creates search based on ``document_class`` parameter, specified
+    in subclass and:
+
+      * filter it by query parameters
+      * apply ordering
+      * filter permissions
+      * apply pagination
+
+    .. IMPORTANT::
+
+        Both ``POST`` and ``GET`` requests are supported.
+    """
+
+    document_class = None
+
+    def custom_filter(self, search):
+        """Perform custom search filtering.
+
+        This method is intended to be overriden in subclasses.
+        """
+        return search
+
+    def list_with_post(self, request):
+        """Endpoint handler."""
+        search = self.document_class().search()  # pylint: disable=not-callable
+
+        search = self.custom_filter(search)
+
+        search = self.filter_search(search)
+        search = self.order_search(search)
+        search = self.filter_permissions(search)
+
+        return self.paginate_response(search)
+
+    def list(self, request):
+        """Endpoint handler."""
+        return self.list_with_post(request)
