265d27157bb0466f5eb1570c39e263187f9a76ad
Jernej Kos <jernej@kos.mx>
2017-04-06 16:00:24 +0200

    Retry slug generation on data races

--- resolwe/flow/models/base.py
+++ resolwe/flow/models/base.py
@@ -7,9 +7,12 @@ from autoslug import AutoSlugField
 from versionfield import VersionField
 
 from django.conf import settings
-from django.db import models
+from django.db import IntegrityError, models, transaction
 
 VERSION_NUMBER_BITS = (8, 10, 14)
+
+# Maximum number of slug-generation retries.
+MAX_SLUG_RETRIES = 10
 
 
 class BaseModel(models.Model):
@@ -44,3 +47,21 @@ class BaseModel(models.Model):
     def __str__(self):
         """Format model name."""
         return self.name
+
+    def save(self, *args, **kwargs):
+        """Save the model."""
+        for _ in range(MAX_SLUG_RETRIES):
+            try:
+                # Attempt to save the model. It may fail due to slug conflict.
+                with transaction.atomic():
+                    super(BaseModel, self).save(*args, **kwargs)
+                    break
+            except IntegrityError as error:
+                # Retry in case of slug conflicts.
+                if '{}_slug'.format(self._meta.db_table) in error.args[0]:
+                    self.slug = None
+                    continue
+
+                raise
+        else:
+            raise IntegrityError("Maximum number of retries exceeded during slug generation")
