ab268cea803920963cbb0e24cd7e8e37de55ee3d
Domen Blenkuš <domen@blenkus.com>
2018-01-26 11:13:25 +0100

    Force channels to respect logging configurations from settings

--- tests/manage.py
+++ tests/manage.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+import channels.log
+import logging
 import os
 import sys
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, PROJECT_ROOT)
+
+
+def get_channels_logger(*args, **kwargs):
+    """Return logger for channels."""
+    return logging.getLogger("django.channels")
+
+
+# Force channels to respect logging configurations from settings:
+# https://github.com/django/channels/issues/520
+channels.log.setup_logger = get_channels_logger
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
