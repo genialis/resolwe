ee35232228b8959bb790b971bf1661b1b3ea41fe
Miha Stajdohar <miha.stajdohar@gmail.com>
2018-10-21 21:36:19 +0200

    Fix logging compatibility with the latest Channels

--- tests/manage.py
+++ tests/manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import channels.log
 import logging
 import os
 import sys
@@ -7,15 +6,6 @@ import sys
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, PROJECT_ROOT)
 
-
-def get_channels_logger(*args, **kwargs):
-    """Return logger for channels."""
-    return logging.getLogger("django.channels")
-
-
-# Force channels to respect logging configurations from settings:
-# https://github.com/django/channels/issues/520
-channels.log.setup_logger = get_channels_logger
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
