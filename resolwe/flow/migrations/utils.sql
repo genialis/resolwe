a6dabf3deb40b3678fc95ffdcaa9cb96ffd06db1
Domen Blenkuš <domen@blenkus.com>
2020-03-03 13:54:41 +0100

    Simplify sql util functions

--- resolwe/flow/migrations/utils.sql
+++ resolwe/flow/migrations/utils.sql
@@ -6,41 +6,33 @@ CREATE TYPE users_result AS (
 
 CREATE OR REPLACE FUNCTION edge_ngrams(text text)
     RETURNS tsvector
-    LANGUAGE plpgsql
+    LANGUAGE sql
     AS $$
-    BEGIN
-        RETURN (
-            SELECT COALESCE(
-                array_to_tsvector((
-                    SELECT array_agg(DISTINCT substring(lexeme for len))
-                    FROM unnest(to_tsvector('simple', text)), generate_series(1,length(lexeme)) len
-                )),
-                ''::tsvector
-            )
-        );
-    END;
+        SELECT COALESCE(
+            array_to_tsvector((
+                SELECT array_agg(DISTINCT substring(lexeme for len))
+                FROM unnest(to_tsvector('simple', text)), generate_series(1,length(lexeme)) len
+            )),
+            ''::tsvector
+        )
     $$;
 
 CREATE OR REPLACE FUNCTION get_characters(text text)
     RETURNS text
-    LANGUAGE plpgsql
+    LANGUAGE sql
     AS $$
-    BEGIN
-        RETURN array_to_string(
+        SELECT array_to_string(
             (SELECT ARRAY (SELECT unnest(regexp_matches(text, '([a-zA-Z]+)', 'g')))),
             ' '
-        );
-    END;
+        )
     $$;
 
 CREATE OR REPLACE FUNCTION get_numbers(text text)
     RETURNS text
-    LANGUAGE plpgsql
+    LANGUAGE sql
     AS $$
-    BEGIN
-        RETURN array_to_string(
+        SELECT array_to_string(
             (SELECT ARRAY (SELECT unnest(regexp_matches(text, '([0-9]+)', 'g')))),
             ' '
-        );
-    END;
+        )
     $$;
