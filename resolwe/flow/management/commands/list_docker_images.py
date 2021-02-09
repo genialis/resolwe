fa4883efa54d4062ba5d2ec273f0b9620a3f4548
Gregor Jerše <gregor.jerse@genialis.com>
2021-02-09 18:31:12 +0100

    Report verbose error if pulling docker image fails

--- resolwe/flow/management/commands/list_docker_images.py
+++ resolwe/flow/management/commands/list_docker_images.py
@@ -85,6 +85,8 @@ class Command(BaseCommand):
             )
         )
 
+        unique_docker_images.add("public.ecr.aws/s4q6j6e8/resolwebio/rnaseq:5.12.0")
+
         # Pull images if requested or just output the list in specified format
         if options["pull"]:
             # Remove set of already pulled images.
@@ -96,18 +98,20 @@ class Command(BaseCommand):
 
             # Pull each image
             for img in unique_docker_images:
-                ret = subprocess.call(
+                ret = subprocess.run(
                     shlex.split("{} pull {}".format(docker, img)),
-                    stdout=None if verbosity > 0 else subprocess.DEVNULL,
-                    stderr=None if verbosity > 0 else subprocess.DEVNULL,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
                 )
 
                 # Update set of pulled images.
                 with PULLED_IMAGES_LOCK:
                     PULLED_IMAGES.add(img)
 
-                if ret != 0:
-                    errmsg = "Failed to pull Docker image '{}'!".format(img)
+                if ret.returncode != 0:
+                    errmsg = "Failed to pull Docker image '{}': {}!".format(
+                        img, ret.stdout
+                    )
 
                     if not options["ignore_pull_errors"]:
                         # Print error and stop execution
