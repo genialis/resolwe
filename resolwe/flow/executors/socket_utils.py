genialis/resolwe

commit 46d7aee7fc9bb3e72d598dc0fd326813769a7eb5

Only log message if necessary

resolwe/flow/executors/socket_utils.py
@@ -940,8 +940,6 @@ class BaseCommunicator:
                     break
 
                 identity, message = received
-                if identity not in self._known_peers:
-                    self.logger.debug("Adding new peer with identity %s.", identity)
 
                 # Only add to known peers if the identity is an integer.
                 # When executor communicates with listener (sending logs for
@@ -950,7 +948,9 @@ class BaseCommunicator:
                 # to monitor them using watchdog.
                 with suppress(ValueError):
                     int(identity)
-                    self._known_peers[identity] = now()
+                    if identity not in self._known_peers:
+                        self.logger.debug("Adding new peer with identity %s.", identity)
+                        self._known_peers[identity] = now()
 
                 if message.message_type is MessageType.HEARTBEAT:
                     # Send response in the background.

commit ffef9d07db7315f95b474dbbe8514c859c1b4411

Set last peer activity on every received message
If only first message is taken into account the peer migth incorrectly
declared as unresponsive. This is a regression caused by the commit
46d7aee7fc9bb3e72d598dc0fd326813769a7eb5.

resolwe/flow/executors/socket_utils.py
@@ -949,7 +949,7 @@ class BaseCommunicator:
                     int(identity)
                     if identity not in self._known_peers:
                         self.logger.debug("Adding new peer with identity %s.", identity)
-                        self._known_peers[identity] = now()
+                    self._known_peers[identity] = now()
 
                 if message.message_type is MessageType.HEARTBEAT:
                     # Send response in the background.