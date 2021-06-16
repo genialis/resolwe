06ac1aae9caf2e1ff63d453a17aec115a837b012
Gregor Jerše <gregor.jerse@genialis.com>
2021-06-16 13:24:06 +0200

    Improve logging in cleanup manager
    so that basic information is logged to level INFO.

--- resolwe/storage/cleanup.py
+++ resolwe/storage/cleanup.py
@@ -5,6 +5,7 @@ from typing import Optional
 from django.db import transaction
 
 from resolwe.storage.models import FileStorage, StorageLocation
+from resolwe.utils import BraceMessage as __
 
 logger = logging.getLogger(__name__)
 
@@ -24,30 +25,36 @@ class Cleaner:
         connector = storage_location.connector
         if connector is None:
             logger.error(
-                "Unable to cleanup StorageLocation {}: connector not found.".format(
-                    storage_location.id
+                __(
+                    "Unable to cleanup StorageLocation {}: connector not found.",
+                    storage_location.id,
                 )
             )
             return
         try:
             # Delete the storage location(files will be also removed).
+            logger.info(__("Deleting StorageLocation {}.", storage_location.pk))
             storage_location.delete()
+
         except Exception:
             logger.exception(
-                "Exception while deleting StorageLocation {}.".format(
-                    storage_location.id
+                __(
+                    "Exception deleting StorageLocation {}.",
+                    storage_location.id,
                 )
             )
 
     def _process_file_storage(self, file_storage: FileStorage):
         """Delete all data from FileStorage object."""
         # Do not remove locked StorageLocation.
+        logger.info(__("Processing FileStorage {}.", file_storage.pk))
         for storage_location in file_storage.storage_locations.filter(
             status=StorageLocation.STATUS_DELETING
         ):
             self._cleanup(storage_location)
 
         if file_storage.storage_locations.count() == 0:
+            logger.info(__("Deleting FileStorage {}.", file_storage.pk))
             file_storage.delete()
 
     def process(self, file_storage_id: Optional[int] = None):
@@ -55,10 +62,13 @@ class Cleaner:
 
         When file_storage is not None process only that object.
         """
-        logger.debug("Starting cleanup manager run")
+
         qset = FileStorage.objects.all()
         if file_storage_id is not None:
+            logger.info(__("Starting processing for FileStorage {}.", file_storage_id))
             qset = qset.filter(pk=file_storage_id)
+        else:
+            logger.info("Starting processing")
         for file_storage in qset.filter(data__isnull=True).iterator():
             # Set applicable storage locations to deleting.
             StorageLocation.all_objects.unreferenced_locations().filter(
@@ -70,7 +80,10 @@ class Cleaner:
                 ).select_for_update(skip_locked=True)
                 # The FileStorage object is locked or deleted, skip processing.
                 if not q_set.exists():
+                    logger.debug(
+                        "Skipping locked or deleted FileStorage {}.", file_storage.id
+                    )
                     continue
                 self._process_file_storage(q_set.first())
 
-        logger.debug("Finished cleanup manager run")
+        logger.info("Finished processing")
