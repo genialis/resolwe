3d5a40def9c55e6cf4b82a7c510e668c13bf7970
Gregor Jerše <gregor.jerse@genialis.com>
2022-02-01 08:42:57 +0100

    Remove redundant Redis settings

--- tests/settings.py
+++ tests/settings.py
@@ -133,7 +133,6 @@ STATIC_URL = "/static/"
 manager_prefix = os.environ.get("RESOLWE_MANAGER_REDIS_PREFIX", "resolwe.flow.manager")
 FLOW_MANAGER = {
     "REDIS_PREFIX": manager_prefix,
-    "REDIS_CONNECTION": REDIS_CONNECTION,
     "TEST": {
         "REDIS_PREFIX": manager_prefix + "-test",
     },
@@ -185,7 +184,6 @@ FLOW_PROCESSES_RUNTIMES = ("resolwe.process.runtime.Process",)
 
 FLOW_EXECUTOR = {
     "NAME": "resolwe.flow.executors.docker",
-    "REDIS_CONNECTION": REDIS_CONNECTION,
     "LISTENER_CONNECTION": LISTENER_CONNECTION,
     "NETWORK": "bridge",
 }
