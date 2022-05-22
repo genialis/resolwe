9bf0dc23a0d05236b902347a3b8544416474a35a
Gregor Jerše <gregor.jerse@genialis.com>
2022-05-22 21:09:34 +0200

    Try to extract id value from models without the id field

--- resolwe/auditlog/models.py
+++ resolwe/auditlog/models.py
@@ -17,8 +17,13 @@ class AuditModel(models.Model):
     @classmethod
     def from_db(cls, db, field_names: List[str], values: List):
         """Override from_db to register read access."""
-        id_index = field_names.index("id")
-        id_value = values[id_index]
+        field_values = dict(zip(field_names, values))
+        id_value = -1
+        id_field_names = ["id", "uuid", "user_id"]
+        for id_field_name in id_field_names:
+            if id_field_name in field_values:
+                id_value = field_values[id_field_name]
+                break
         AuditManager.register_access(cls, id_value, AccessType.READ, set(field_names))
         return super().from_db(db, field_names, values)
 
