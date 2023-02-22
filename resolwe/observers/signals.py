1d558ae2bfdc7e876ab62334f27a61e68cb7bcaf
Gregor Jerše <gregor.jerse@genialis.com>
2023-02-22 13:25:26 +0100

    Add skip_in_migrations decorator
    to simplify observer signals code

--- resolwe/observers/signals.py
+++ resolwe/observers/signals.py
@@ -35,58 +35,69 @@ def model_post_migrate(*args, **kwargs):
     IN_MIGRATIONS = False
 
 
+def skip_in_migrations(function):
+    """Execute the given method if not in migrations."""
+
+    def wrapped(*args, **kwargs):
+        if not IN_MIGRATIONS:
+            function(*args, **kwargs)
+
+    return wrapped
+
+
 @dispatch.receiver(pre_permission_changed)
+@skip_in_migrations
 def prepare_permission_change(instance, **kwargs):
     """Store old permissions for an object whose permissions are about to change."""
-    if not IN_MIGRATIONS:
-        instance._old_viewers = instance.users_with_permission(
-            Permission.VIEW, with_superusers=True
-        )
+    instance._old_viewers = instance.users_with_permission(
+        Permission.VIEW, with_superusers=True
+    )
 
 
 @dispatch.receiver(pre_container_changed)
+@skip_in_migrations
 def prepare_container_change(instance, **kwargs):
     """Store old containerds for an object."""
-    if not IN_MIGRATIONS:
-        prepare_permission_change(instance)
-        instance._old_containers = set(instance.containers)
+    prepare_permission_change(instance)
+    instance._old_containers = set(instance.containers)
 
 
 @dispatch.receiver(post_container_changed)
+@skip_in_migrations
 def handle_container_change(instance, **kwargs):
     """Send notifications when object is moved to another container."""
-    if not IN_MIGRATIONS:
-        old_containers = getattr(instance, "_old_containers", set())
-        new_containers = set(instance.containers)
-        removed_from = old_containers - new_containers
-        added_to = new_containers - old_containers
+    old_containers = getattr(instance, "_old_containers", set())
+    new_containers = set(instance.containers)
+    removed_from = old_containers - new_containers
+    added_to = new_containers - old_containers
 
-        # Handle possible permission change (CREATE/DELETE) on the object itself. Do
-        # not send notifications on the containers, this is handled bellow.
-        handle_permission_change(instance, observe_containers=False)
-        # Handle notifications (CREATE/DELETE) on the containers.
-        Observer.observe_instance_container(instance, ChangeType.DELETE, removed_from)
-        Observer.observe_instance_container(instance, ChangeType.CREATE, added_to)
-        # Handle UPDATE notifications on the instance (container property has changed).
-        Observer.observe_instance_changes(instance, ChangeType.UPDATE)
+    # Handle possible permission change (CREATE/DELETE) on the object itself. Do
+    # not send notifications on the containers, this is handled bellow.
+    handle_permission_change(instance, observe_containers=False)
+    # Handle notifications (CREATE/DELETE) on the containers.
+    Observer.observe_instance_container(instance, ChangeType.DELETE, removed_from)
+    Observer.observe_instance_container(instance, ChangeType.CREATE, added_to)
+    # Handle UPDATE notifications on the instance (container property has changed).
+    Observer.observe_instance_changes(instance, ChangeType.UPDATE)
 
 
 @dispatch.receiver(post_permission_changed)
+@skip_in_migrations
 def handle_permission_change(instance, **kwargs):
     """Compare permissions for an object whose permissions changed."""
-    if not IN_MIGRATIONS:
-        new = set(instance.users_with_permission(Permission.VIEW, with_superusers=True))
-        # The "_old_viewers" property may not exist. For instance if data object is
-        # created in the collection and collection permissions are assigned to it
-        # without set_permission call.
-        old = set(getattr(instance, "_old_viewers", []))
-        gains = new - old
-        losses = old - new
-        observe_containers = kwargs.get("observe_containers", True)
-        Observer.observe_permission_changes(instance, gains, losses, observe_containers)
+    new = set(instance.users_with_permission(Permission.VIEW, with_superusers=True))
+    # The "_old_viewers" property may not exist. For instance if data object is
+    # created in the collection and collection permissions are assigned to it
+    # without set_permission call.
+    old = set(getattr(instance, "_old_viewers", []))
+    gains = new - old
+    losses = old - new
+    observe_containers = kwargs.get("observe_containers", True)
+    Observer.observe_permission_changes(instance, gains, losses, observe_containers)
 
 
 @dispatch.receiver(model_signals.post_save)
+@skip_in_migrations
 def observe_model_modification(
     sender: type, instance: Model, created: bool = False, **kwargs
 ):
@@ -98,17 +109,18 @@ def observe_model_modification(
 
     Do not send notifications to the containers when object is moving between them.
     """
-    if not IN_MIGRATIONS and isinstance(instance, Observable):
+    if isinstance(instance, Observable):
         if created:
             handle_permission_change(instance)
-    elif not getattr(instance, suppress_notifications_attribute, False):
-        Observer.observe_instance_changes(instance, ChangeType.UPDATE)
-        Observer.observe_instance_container(instance, ChangeType.UPDATE)
+        elif not getattr(instance, suppress_notifications_attribute, False):
+            Observer.observe_instance_changes(instance, ChangeType.UPDATE)
+            Observer.observe_instance_container(instance, ChangeType.UPDATE)
 
 
 @dispatch.receiver(model_signals.pre_delete)
+@skip_in_migrations
 def observe_model_deletion(sender: type, instance: Model, **kwargs):
     """Receive model deletions."""
-    if isinstance(instance, Observable) and not IN_MIGRATIONS:
+    if isinstance(instance, Observable):
         Observer.observe_instance_changes(instance, ChangeType.DELETE)
         Observer.observe_instance_container(instance, ChangeType.DELETE)
