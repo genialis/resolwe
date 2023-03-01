cb4f20285ea1637c7273f6239258d82ce7bf9ca2
Gregor Jerše <gregor.jerse@genialis.com>
2023-03-01 10:33:59 +0100

    Create and register background task endpoint

--- resolwe/api_urls.py
+++ resolwe/api_urls.py
@@ -12,6 +12,7 @@ from .flow.views import (
     RelationViewSet,
     StorageViewSet,
 )
+from .observers.views import BackgroundTaksViewSet
 from .storage.views import UploadConfig
 
 api_router = routers.DefaultRouter(trailing_slash=False)
@@ -23,6 +24,7 @@ api_router.register(r"relation", RelationViewSet)
 api_router.register(r"descriptorschema", DescriptorSchemaViewSet)
 api_router.register(r"storage", StorageViewSet)
 api_router.register(r"upload_config", UploadConfig, basename="upload_config")
+api_router.register(r"task", BackgroundTaksViewSet, basename="backgroundtask")
 
 
 urlpatterns = [
