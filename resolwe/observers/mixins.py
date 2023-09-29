25dfe2efa088d60746a2d863db6ffcff2a5c6dbe
Yon Ploj <yon.ploj@genialis.com>
2023-09-29 10:22:56 +0200

    Define schema for Observers

--- resolwe/observers/mixins.py
+++ resolwe/observers/mixins.py
@@ -1,7 +1,10 @@
 """Mixins for Observable ViewSets."""
+from drf_spectacular.utils import extend_schema
+
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
+from rest_framework import serializers
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
@@ -11,6 +14,21 @@ from resolwe.permissions.models import get_anonymous_user
 
 from .models import Observer, Subscription
 from .protocol import ChangeType
+
+
+class ObserverSubscribeRequestSerializer(serializers.Serializer):
+    """Serializer for the subscribe endpoint."""
+
+    ids = serializers.ListField(
+        child=serializers.IntegerField(), required=False, allow_null=True
+    )
+    session_id = serializers.CharField()
+
+
+class ObserverSubscriptionSerializer(serializers.Serializer):
+    """Serializer for the subscribe response."""
+
+    subscription_id = serializers.UUIDField()
 
 
 class ObservableMixin:
@@ -23,11 +41,18 @@ class ObservableMixin:
         """Verify that an object exists for a given user."""
         return self.get_queryset().filter(pk=id).filter_for_user(user).exists()
 
+    @extend_schema(
+        request=ObserverSubscribeRequestSerializer,
+        responses={200: ObserverSubscriptionSerializer},
+    )
     @action(detail=False, methods=["post"])
     def subscribe(self, request: Request) -> Response:
         """Register an Observer for a resource."""
-        ids = request.data.get("ids", None)
-        session_id = request.data.get("session_id")
+        deserializer = ObserverSubscribeRequestSerializer(data=request.data)
+        deserializer.is_valid(raise_exception=True)
+        ids = deserializer.validated_data.get("ids")
+        session_id = deserializer.validated_data["session_id"]
+
         content_type = ContentType.objects.get_for_model(self.get_queryset().model)
         user = request.user if request.user.is_authenticated else get_anonymous_user()
         subscription = Subscription.objects.create(user=user, session_id=session_id)
@@ -46,13 +71,19 @@ class ObservableMixin:
             change_types = (ChangeType.UPDATE, ChangeType.DELETE, ChangeType.CREATE)
             subscription.subscribe(content_type, ids, change_types)
 
-        resp = {"subscription_id": subscription.subscription_id}
-        return Response(resp)
+        serializer = ObserverSubscriptionSerializer(
+            {"subscription_id": subscription.subscription_id}
+        )
+        return Response(serializer.data)
 
+    @extend_schema(request=ObserverSubscriptionSerializer, responses={200: None})
     @action(detail=False, methods=["post"])
     def unsubscribe(self, request: Request) -> Response:
         """Unregister a subscription."""
-        subscription_id = request.data.get("subscription_id", None)
+        deserializer = ObserverSubscriptionSerializer(data=request.data)
+        deserializer.is_valid(raise_exception=True)
+        subscription_id = deserializer.validated_data["subscription_id"]
+
         user = request.user if request.user.is_authenticated else get_anonymous_user()
         subscription = Subscription.objects.filter(
             subscription_id=subscription_id, user=user
