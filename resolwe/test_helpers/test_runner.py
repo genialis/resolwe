c8b2e9bc8fe49a6319060d5c12b5bc2db3845983
Gregor Jerše <gregor.jerse@genialis.com>
2024-06-13 18:43:08 +0200

    Fix authentication needs extra time to stop
    or starting it again will raise exception

--- resolwe/test_helpers/test_runner.py
+++ resolwe/test_helpers/test_runner.py
@@ -329,6 +329,8 @@ async def _run_on_infrastructure(meth, *args, **kwargs):
                         logger.exception("Exception while running test")
                     finally:
                         zmq_info.authenticator.stop()
+                        # Give authenticator task some time to finish.
+                        await asyncio.sleep(0.1)
                         logger.debug("test_runner: Terminating listener")
 
 
