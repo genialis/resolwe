8070dbea0a6605b7aeef6074474bf13b9e261bd8
Gregor Jerše <gregor.jerse@genialis.com>
2024-10-16 07:59:51 +0200

    Test

--- resolwe/flow/models/base.py
+++ resolwe/flow/models/base.py
@@ -1,5 +1,7 @@
 """Base model for all Resolwe models."""
 
+import datetime
+
 from django.conf import settings
 from django.db import IntegrityError, models, transaction
 from versionfield import VersionField
@@ -62,6 +64,65 @@ def delete_chunked(queryset, chunk_size=500):
             queryset.filter(pk__lte=last_instance.pk).delete()
 
 
+class BaseManager[M: models.Model, Q: models.QuerySet](models.Manager[M]):
+    """Base query set for Resolwe's ORM objects."""
+
+    # The data is partitioned into groups with the same partition fields value. The
+    # object with the greatest version inside the partition is considered the latest.
+    partition_fields: list[str] = ["slug", "version"]
+
+    # The field to use as version. The latest version is determined by sorting the
+    # data by version field in the ascending order and taking the first entry.
+    version_field: str = "version"
+
+    # The created field is used to determine the version at certain point in time.
+    created_field: str = "created"
+
+    # The queryset to use.
+    QuerySet: type[Q] = models.QuerySet
+
+    def _only_latest(self, queryset: Q) -> Q:
+        """Return only latest values from the queryset.
+
+        The queryset is partitioned by the partition fields and the latest version from
+        each partition is returned.
+        """
+        latest_entries = queryset.annotate(
+            rank=models.Window(
+                expression=models.functions.DenseRank(),
+                partition_by=[models.F(field) for field in self.partition_fields],
+                order_by=models.F(self.version_field).desc(),
+            ),
+        ).filter(rank=1)
+        return queryset.filter(pk__in=latest_entries)
+
+    def get_queryset(self) -> Q:
+        """Return only the latest version for every field.
+
+        Make sure the queryset is lazily evaluated, since the get_queryset method can
+        be called many times (for instance, when filtering the queryset).
+        """
+        base_queryset = self.QuerySet(model=self.model, using=self._db)
+        return self._only_latest(base_queryset)
+
+    def history(self, timestamp: datetime.datetime) -> Q:
+        """Get values at certain point in time"""
+        created_filter = {f"{self.created_field}__le": timestamp}
+        filtered_queryset: Q = self.filter(**created_filter)  # type: ignore
+        return self._only_latest(filtered_queryset)
+
+    def delete_chunked(self, chunk_size=500):
+        """Chunked delete, which should be used if deleting many objects.
+
+        The reason why this method is needed is that deleting a lot of Data objects
+        requires Django to fetch all of them into memory (fast path is not used) and
+        this causes huge memory usage (and possibly OOM).
+
+        :param chunk_size: Optional chunk size
+        """
+        return delete_chunked(self, chunk_size=chunk_size)
+
+
 class BaseQuerySet(models.QuerySet):
     """Base query set for Resolwe's ORM objects."""
 
@@ -107,6 +168,12 @@ class BaseModel(AuditModel):
 
     #: user that created the entry
     contributor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    #: the latest version of the fields
+    objects = BaseManager()
+
+    #: all fields
+    all_objects = models.Manager()
 
     def __str__(self):
         """Format model name."""
